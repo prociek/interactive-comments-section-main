67bd45c4c87d4ab87cbe61d6eddd7a848044b74f
Prociek <prociek@poczta.onet.pl>
2023-06-28 22:11:23 +0200

    added react template

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Tomasz Procko</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
